e5f701aa756eff3708b514c108fa4c9a9dfd3308
Daniel Narváez <fernandonarvaezr@gmail.com>
2026-04-30 12:20:15 -0500

    clases base

---
+++ clases/entidad_base.py
@@ -0,0 +1,7 @@
+class EntidadBase():
+    
+    def __int__(self, id, nombre, fecha_creacion):
+        self.id = id
+        self.nombre = nombre
+        self.fecha_creacion = fecha_creacion
+        pass
---
+++ clases/servicio.py
@@ -0,0 +1,3 @@
+
+class Servicio():
+    pass
