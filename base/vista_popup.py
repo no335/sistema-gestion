6df0f90963f741915a0cc63ac3a76a1644986eee
Daniel Narváez <fernandonarvaezr@gmail.com>
2026-05-12 14:20:46 -0500

    cargar formulario de objeto en base

--- base/vista_popup.py
+++ base/vista_popup.py
@@ -2,6 +2,11 @@
 # utlilza tkinter
 import tkinter as tk
 from tkinter import ttk
+# logger para guardar mensajes del sistema
+import logging
+# se crea o se obtiene una instancia de logger
+# relacionada con el nombre del módulo
+logger = logging.getLogger(__name__)
 
 # generar clase
 class BasePopup():
@@ -40,4 +45,34 @@ class BasePopup():
         # agregar boton para cerrar
         crear_button = ttk.Button(self.error, text="Aceptar", command=lambda: self.error.destroy())
         crear_button.pack(pady=5, padx=5)
+
+    def cargar_formulario(self, entidad, formulario):
+        """Recibe una instancia de entidad y llena el formulario con los
+        valores que tiene este objeto"""
+        # inicializar valores en el formulario
+        # tomándolos del objeto cliente
+        for propiedad, valor in entidad.diccionario().items():
+            if propiedad not in formulario or  valor is None:
+                continue
+            if isinstance(formulario[propiedad], tk.StringVar):
+                logger.debug('%s is StrVar' % propiedad)
+                formulario[propiedad].set(valor)
+            elif isinstance(formulario[propiedad], ttk.Combobox):
+                logger.debug('%s is Combobox' % propiedad)
+                formulario[propiedad].set(valor)
+            elif isinstance(formulario[propiedad], tk.IntVar):
+                logger.debug('%s is IntVar' % propiedad)
+                try: 
+                    formulario[propiedad].set(int(valor))
+                except (ValueError, TypeError):
+                    logger.error(f"CompraPopup.cargar_formulario: Valor Int inválido para {propiedad}")
+                    formulario[propiedad].set(False)
+
+            elif isinstance(formulario[propiedad], tk.BooleanVar):
+                try:
+                    formulario[propiedad].set(bool(int(valor)))
+                except (ValueError, TypeError):
+                    logger.error(f"CompraPopup.cargar_formulario: Valor Bool inválido para {propiedad}")
+                    formulario[propiedad].set(False)
+
 
