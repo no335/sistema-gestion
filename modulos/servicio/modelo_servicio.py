c9e453195ba1c00b95ea0107f2973baf8ef84d45
Daniel Narváez <fernandonarvaezr@gmail.com>
2026-05-12 16:30:52 -0500

    corrección servicios

--- modulos/servicio/modelo_servicio.py
+++ modulos/servicio/modelo_servicio.py
@@ -4,7 +4,7 @@ class Servicio(Entidad):
     _nombre_entidad = "servicio"
     
     def __init__(self, descripcion=None, costo=None, responsable=None, activo=None, **kwargs):
-        super().__init__(kwargs)
+        super().__init__(**kwargs)
         self.descripcion = descripcion
         self.costo = costo
         self.responsable = responsable
