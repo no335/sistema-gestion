6c41c724bd1d295584c426a63a2b3eb45a04fafa
Daniel Narváez <fernandonarvaezr@gmail.com>
2026-05-12 16:12:08 -0500

    vista popup

--- modulos/servicio/modelo_servicio.py
+++ modulos/servicio/modelo_servicio.py
@@ -2,4 +2,12 @@ from base.modelo_entidad import Entidad
 
 class Servicio(Entidad):
     _nombre_entidad = "servicio"
-    pass
+    
+    def __init__(self, descripcion=None, costo=None, responsable=None, activo=None, **kwargs):
+        super().__init__(kwargs)
+        self.descripcion = descripcion
+        self.costo = costo
+        self.responsable = responsable
+        self.activo = activo
+
+Servicio.agregar_columnas('descripcion', 'costo', 'responsable', 'activo')
