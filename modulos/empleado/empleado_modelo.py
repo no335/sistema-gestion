d82a29b5b09a5a81bdd035c17bea9111446be3e7
Daniel Narváez <fernandonarvaezr@gmail.com>
2026-05-12 14:19:58 -0500

    modulo empleados

--- modulos/empleado/empleado_modelo.py
+++ modulos/empleado/empleado_modelo.py
@@ -1,4 +1,11 @@
 from ..login.modelo_usuario import Usuario
 
 class Empleado(Usuario):
-    pass
+    
+    def __init__(self, tipo=None, jefe_inmediato=None, bloqueado=None, **kwargs):
+        super().__init__(**kwargs)
+        self.tipo = tipo
+        self.jefe_inmediato = jefe_inmediato
+        self.bloqueado = bloqueado
+
+Empleado.agregar_columnas('tipo', 'jefe_inmediato', 'bloqueado')
